c8138d795353ec53dacd4a2068415a726a11cca1
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2019-04-14 19:35:22 -0300

    Classe Concessionaria - Praticando mais um pouco

--- angular/typescript/app2.ts
+++ angular/typescript/app2.ts
@@ -21,11 +21,22 @@ class Carro {
     }
 }
 
-let carroA = new Carro("Veloster", 3)
+class Concessionaria {
+    private endereco: string
+    private listaDeCarros: any
 
-console.log(carroA)
-carroA.acelerar()
-console.log(carroA)
-carroA.acelerar()
-carroA.acelerar()
-console.log(carroA)
+    constructor(endereco: string) {
+        this.endereco = endereco
+    }
+
+    public fornecerEndereco(): string {
+        return this.endereco
+    }
+
+    public mostrarListaDeCarros(): any {
+        return this.listaDeCarros
+    }
+}
+
+let concessionaria = new Concessionaria('Av Paulista')
+console.log(concessionaria)
