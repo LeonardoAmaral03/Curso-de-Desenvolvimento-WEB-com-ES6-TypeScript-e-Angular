8bc8c884df88b3dbd4d10d5336b00303ace3523d
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2019-04-14 18:22:05 -0300

    Inferência de tipos

--- angular/typescript/app.ts
+++ angular/typescript/app.ts
@@ -1,5 +1,8 @@
-let ola = (nome: string, sobrenome: string) => {
-    console.log("Olá " + nome + " " + sobrenome)
-}
+let mensagem: string = "Seja bem vindo!!!"
+let temporadasFriends: number = 10
+let estudandoAngular: boolean = true
 
-ola("Leonardo", "Amaral da Rocha")
+let listaDeFrutas: Array<string> = ["Uva", "Banana", "Abacaxi"]
+let listaDeFrutas2: string[] = ["Uva", "Banana", "Abacaxi"]
+
+let notasDasProvas: Array<number> = [7.5, 8, 9]
--- angular/typescript/dist/app.js
+++ angular/typescript/dist/app.js
@@ -1,5 +1,7 @@
 "use strict";
-var ola = function (nome, sobrenome) {
-    console.log("Olá " + nome + " " + sobrenome);
-};
-ola("Leonardo", "Amaral da Rocha");
+var mensagem = "Seja bem vindo!!!";
+var temporadasFriends = 10;
+var estudandoAngular = true;
+var listaDeFrutas = ["Uva", "Banana", "Abacaxi"];
+var listaDeFrutas2 = ["Uva", "Banana", "Abacaxi"];
+var notasDasProvas = [7.5, 8, 9];
