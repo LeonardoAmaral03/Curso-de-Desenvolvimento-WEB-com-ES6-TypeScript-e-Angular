d2da12a3852a65b8a740e474059bf0c8bc14052b
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2019-04-14 14:11:06 -0300

    Arrow functions

---
+++ angular/es6/arrow_function.js
@@ -0,0 +1,28 @@
+/*
+var dobroDoValor = function(numero) {
+    return numero * 2
+}
+*/
+
+/*
+var dobroDoValor = (numero) => {
+    return numero * 2
+}
+*/
+
+/*
+var dobroDoValor = () => {
+    return numero * 2
+}
+*/
+
+/*
+var dobroDoValor = numero => {
+    let resultado = numero * 2
+    return resultado
+}
+*/
+
+var dobroDoValor = numero => numero * 2
+
+console.log(dobroDoValor(7))
--- angular/es6/variavel.js
+++ angular/es6/variavel.js
@@ -1,6 +1,6 @@
 const serie = "Friends"
 
-serie = "The Walking Dead"
+//serie = "The Walking Dead"
 
 if(true){
     let serie = "Game Of Thrones"
